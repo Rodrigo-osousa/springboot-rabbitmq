dc6492fe37a3b2f040fb11ef5505d81d3eb27adc
Rodrigo O.Sousa <rodrigo_oliveira_sousa@hotmail.com>
2022-04-01 13:09:53 -0300

    Adicionado Logger para notificar quando uma mensagem foi enviada

--- src/main/java/com/springbootrabbitmq/controller/IventaryController.java
+++ src/main/java/com/springbootrabbitmq/controller/IventaryController.java
@@ -11,15 +11,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping(value = "/iventary")
 public class IventaryController {
+
+    private static final Logger LOGGER = Logger.getLogger(IventaryController.class.getName());
 
     @Autowired
     private RabbitmqService rabbitmqService;
 
     @PutMapping
     private ResponseEntity changeIventary(@RequestBody IventaryDto iventaryDto) {
+        LOGGER.info("New Iventary message created");
         this.rabbitmqService.sendMessage(RabbitmqConstants.QUEUE_INVENTORY, iventaryDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
--- src/main/java/com/springbootrabbitmq/controller/PriceController.java
+++ src/main/java/com/springbootrabbitmq/controller/PriceController.java
@@ -12,16 +12,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping(value = "/price")
 public class PriceController {
 
+    private static final Logger LOGGER = Logger.getLogger(PriceController.class.getName());
 
     @Autowired
     private RabbitmqService rabbitmqService;
 
     @PutMapping
     private ResponseEntity changePrice(@RequestBody PriceDto priceDto) {
+        LOGGER.info("New Price message created");
         this.rabbitmqService.sendMessage(RabbitmqConstants.QUEUE_PRICE, priceDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
